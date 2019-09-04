3cedff187791f6101ddd2b956fdbfe81384d39e8
Rick Gleason <fcgleason@tgp-architects.com>
2019-09-04 14:07:05 -0400

    version v1.0.25

--- VERSION.cmake
+++ VERSION.cmake
@@ -6,7 +6,7 @@ SET(OCPN_MIN_VERSION "ov50" )
 SET(PI_COMMENT "  * Initial release for O5 using CI" )
 SET(OCPN_API_VERSION_MAJOR "1" )
 SET(OCPN_API_VERSION_MINOR "12" )
-##############################
+###############################
 SET(PLUGIN_VERSION_MAJOR "${VERSION_MAJOR}" )
 SET(PLUGIN_VERSION_MINOR "${VERSION_MINOR}" )
 SET(PLUGIN_VERSION_PATCH "${VERSION_PATCH}" )
